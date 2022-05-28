3357a0186c44a193c8f34e2ee38abd76dd77d8a0
michaelgreenl <lgreener25@gmail.com>
2022-05-28 13:52:24 -0400

    Added components for each of the algorithms and data structures

---
+++ components/Algorithms/InsertionSort.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="insertion-sort">
+    <VisualizerDSA :options="options" />
+  </div>  
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+const options = reactive([
+  { label: 'speed', input: 'slider', default: '50%' }
+]);
+</script>
+
+<style lang="scss" scoped></style>
